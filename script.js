7702c014b7811b9bc3a476833ae805eea8073748
Siamorim <77587204+Siamorim@users.noreply.github.com>
2021-01-27 21:52:43 +0000

    Update script.js

--- script.js
+++ script.js
@@ -114,7 +114,7 @@ function searchCity(city) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(showTemperature);
 
-  apiUrl = `https://openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+  apiUrl = `https://openweathermap.org/forecast5?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 
