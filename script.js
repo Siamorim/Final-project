fb131c34dfd212821b81237ceaa57d7efabc8e68
Siamorim <77587204+Siamorim@users.noreply.github.com>
2021-01-28 19:51:51 +0000

    Update script.js

--- script.js
+++ script.js
@@ -158,7 +158,7 @@ function displayForecast(response) {
   let forecast = null;
   let maxTemperature = Math.round(forecast.main.temp_max);
   let minTemperature = Math.round(forecast.main.temp_min);
-
+  console.log(response.data.list);
   forecastElement.innerHTML = null;
 
   for (let index = 2; index < 35; index += 8) {
