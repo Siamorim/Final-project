f93b40eb6344cbe2a05ff48c29f02b8a53d7467c
Siamorim <77587204+Siamorim@users.noreply.github.com>
2021-01-28 00:22:43 +0000

    Update script.js

--- script.js
+++ script.js
@@ -125,19 +125,12 @@ function displayForecast(response) {
              <div class="MinMax">${Math.round(forecast.main.temp)}º</div></div>
              `;
 
-  // forecast = response.data.list[1];
-  //forecastElement.innerHTML += `
-  //  <div class="row">
-  //  <div class="col-2">${formatHours(forecast.dt * 1000)}</div>
-  //<div class="w-100"></div>
-  //<div class="col-2">
-  //<small><img src=${`Icon/${forecast.weather[0].icon}.png`} class="Sunn" /></small>
-  //</div>
-  //<div class="w-100"></div>
-  //<div class="col-2"><div class="MinMax">${Math.round(
-  // forecast.main.temp
-  //)}º</div></div>
-  // `;
+  forecast = response.data.list[1];
+  forecastElement.innerHTML += `
+  <div class="col-2"><div>${formatHours(forecast.dt * 1000)}</div>
+            <img src=${`Icon/${forecast.weather[0].icon}.png`} class="Sunn" />
+             <div class="MinMax">${Math.round(forecast.main.temp)}º</div></div>
+             `;
 }
 
 function searchCity(city) {
