d813af4d96397e085beed209e2d75a70a6593db0
Siamorim <77587204+Siamorim@users.noreply.github.com>
2021-01-28 20:19:03 +0000

    Update script.js

--- script.js
+++ script.js
@@ -159,7 +159,7 @@ function displayForecast(response) {
   forecastElement.innerHTML = null;
   console.log(response.data.list);
 
-  for (let index = 0; index < 35; index += 8) {
+  for (let index = 2; index < 40; index += 8) {
     forecast = response.data.list[index];
     forecastElement.innerHTML += `
   
