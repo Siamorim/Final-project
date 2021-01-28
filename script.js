3c2f4e4f304077b07b5c8d5d98b548908d5774c2
Siamorim <77587204+Siamorim@users.noreply.github.com>
2021-01-28 00:24:23 +0000

    Update script.js

--- script.js
+++ script.js
@@ -120,9 +120,11 @@ function displayForecast(response) {
   let forecast = response.data.list[0];
   console.log(forecast);
   forecastElement.innerHTML = `  
-            <div class="col-2"><div>${formatHours(forecast.dt * 1000)}</div>
+            <div class="col-2">
+            <div>${formatHours(forecast.dt * 1000)}</div>
             <img src=${`Icon/${forecast.weather[0].icon}.png`} class="Sunn" />
-             <div class="MinMax">${Math.round(forecast.main.temp)}º</div></div>
+             <div class="MinMax">${Math.round(forecast.main.temp)}º</div>
+             </div>
              `;
 
   forecast = response.data.list[1];
