69d0431ed2a02aa8fc7aa3a7baf2696a3608d67e
Siamorim <77587204+Siamorim@users.noreply.github.com>
2021-01-28 13:24:42 +0000

    Update script.js

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ function formatDate(timestamp) {
   todayDay.innerHTML = `${currentDay}, ${currentMonth} ${currentDate} ${currentFullYear}, ${currentHour}:${currentMinute}`;
 }
 
-let date = new Date();
+let date = new Date(timestamp);
 formatDate(date);
 
 function convertToFahrenheit(event) {
